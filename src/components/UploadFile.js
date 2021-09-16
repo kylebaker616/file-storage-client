44e43297bdd1033817c4fd883fcbe5ba8a0a0fea
kylebaker616 <kylebaker616@gmail.com>
2021-09-15 20:10:34 -0400

    Add state for loading display

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -8,6 +8,7 @@ import { FormData } from 'formdata-node'
 function UploadFile () {
   const [selected, setSelected] = useState({})
   const [upload, setUpload] = useState({})
+  const [loading, setLoading] = useState(false)
   const handleChange = (event) => {
     console.log(event.target.files)
     setSelected(event.target.files[0])
@@ -15,6 +16,7 @@ function UploadFile () {
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    setLoading(true)
     console.log('hello', event.target)
     const data = new FormData()
     // upload is the key that the api is expecting, value is the selected file set in state
@@ -22,6 +24,7 @@ function UploadFile () {
     console.log(data, 'ooo')
     uploadFile(data)
       .then(res => setUpload(res.data.uploadDoc))
+      .then(() => setLoading(false))
       .catch(console.error + 'oops')
   }
 
@@ -29,6 +32,7 @@ function UploadFile () {
     <Fragment>
       {/* ternary operator displays the image that was just uploaded by setUploaded after the axios call */}
       {upload.url ? (<img className="display-image" alt={upload.url} src={upload.url}/>) : '' }
+      {loading ? (<img alt="loading gif" src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"/>) : ''}
       <form onSubmit={handleSubmit}>
         <Form.Group controlId='formFile' className='mb-3'>
           <Form.Label>Upload file here</Form.Label>
