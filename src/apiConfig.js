85a3ff010f941561c379eee0c125fa3b3f970a57
kylebaker616 <kylebaker616@gmail.com>
2021-09-20 20:19:01 -0400

    Add depolyed api URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://pacific-lowlands-77744.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
